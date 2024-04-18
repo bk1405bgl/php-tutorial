ff58202c0c584d0d2cc5fce8cb59fec01a7c269a
Bilal Kuzey <bk1405bgl@gmail.com>
2024-04-18 02:11:31 -0700

    conditions

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="de">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello</title>
-</head>
-<body>
-    <h1>This is the title</h1>
-    <?php echo "Hello World"; ?>
-</body>
-</html>
+<?php
+    $result = 1;
+    echo $result == false;
