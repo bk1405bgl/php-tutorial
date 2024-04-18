2287e0ab543fc705cc17e7c0e8306313cc874cdb
Bilal Kuzey <bk1405bgl@gmail.com>
2024-04-18 01:56:46 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello</title>
+</head>
+<body>
+    <h1>This is the title</h1>
+    <?php echo "Hello World"; ?>
+</body>
+</html>
