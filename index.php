5c1a49070c355c3b35ecfe83f5a2a314665dd517
Bilal Kuzey <bk1405bgl@gmail.com>
2024-04-23 09:05:13 -0700

    tailwind css testing

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-    echo "Hello";
-?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="output.css">
+    <title>Tutorial Nachbau</title>
+</head>
+<body>
+    <div class="bg-black text-white">
+        <?= "Hello"; ?>
+    </div>
+</body>
+</html>
